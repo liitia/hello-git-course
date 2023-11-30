8ce728993af469329491de75c1073e7b68846613
Liinu Tiainen <tiainen.liinu@gmail.com>
2023-11-30 14:50:14 +0200

    Add prints
    {develhaara}
    
    {devellisötty}
    
    {kansio}
    
    {kaikkilisätty}
    
    lisätty rivi 3
    
    tehtävä 2
    
    tehtävä 2
    
    tehtävä 3
    
    Delete hello_world.py
    
    Delete second.py
    
    Delete intermediate-normal-merge-b23547b242012497282910300f6e6c1d5a27df82.zip
    
    {lisäys}
    
    poistetut
    
    Rename hello_world (8).py to hello_world.py
    
    Delete advanced-rebase-master directory
    
    Delete hello_world (9).py
    
    Delete hello_world (14).py
    
    Delete hello_world (13).py
    
    Delete hello_world (12).py
    
    Delete hello_world (11).py
    
    Delete hello_world (10).py
    
    Add prints
    
    Add prints
    
    Add prints
    
    Add prints

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,19 @@
 print("Hello world!")
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+print("Not another hello world")
+print("No")
+print("More")
+print("Hello")
+print("World")
+print("Please, I'm begging you")
+=======
+>>>>>>> ce487ba ({kaikkilisätty})
+=======
+=======
+print("Hello darkness my old friend")
+>>>>>>> 255d1ea (tiedoston lisääminen)
+>>>>>>> cc54cbc (Add prints)
+=======
+>>>>>>> c0e856c (Add prints)
